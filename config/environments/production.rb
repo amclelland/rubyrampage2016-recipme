2f202deaa8bfb793487527e7c7fd0c6f98be7b3e
Allan McLelland <allan.mclelland@protrainings.com>
2016-10-22 16:47:57 -0600

    Add Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.action_mailer.default_url_options = { host: 'http://recipme.2016.rubyrampage.com', port: 80 }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
